a780284bd962f3cc3b18b9607f835d8b9cbb1f45
varun <varunnssaini@gmail.com>
2021-04-01 10:13:19 +0530

    Connected Database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const Joi = require('@hapi/joi');
 const movies = require('./movies');
+const dbService = require('./dbService');
 
 app.use(express.json());
 
@@ -10,14 +11,22 @@ app.use(express.static(__dirname + '/views'));
 
 app.use('/abc', movies);
 
-
-// app.get('/', (req,res) => {
-//     res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
-// })
-
 app.get("/", (req, res) => {
     res.render("index");
   });
+
+app.get('/getAll', (request, response) => {
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.getAllData();
+  result
+  .then(data => {
+          response.json({data : data})
+      })
+  .catch(err => console.log(err));
+})
+
+
 
 
 
@@ -30,3 +39,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
+
