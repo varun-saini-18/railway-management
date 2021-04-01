8a022a458075acd103119c217dd621df2ed41662
varun <varunnssaini@gmail.com>
2021-04-01 10:30:41 +0530

    Added ejs files

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -1,7 +1,7 @@
 console.log('Yes');
 
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('http://localhost:5000/getAll')
+    fetch('/getAll')
     .then(response => response.json())
     .then(data => loadHTMLTable(data.data));
 
