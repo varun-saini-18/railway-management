791e55afbd1ac7231c31dbc1276f3728d24ab859
varun <varunnssaini@gmail.com>
2021-04-06 17:41:17 +0530

    Fixed issue related to arrival and departure time

--- views/scripts/ticketsDetail.js
+++ views/scripts/ticketsDetail.js
@@ -11,11 +11,11 @@ function fillTicketDetail() {
         {
             setTimeout(function(){ document.getElementById("ticket-id").innerHTML = data[0].ticket_id; },10);
             setTimeout(function(){ document.getElementById("train-num").innerHTML = `<a href="/traindetail/${data[0].train_num}">${data[0].train_num}</a>`; },500);
-            setTimeout(function(){ document.getElementById("src").innerHTML = data[0].src; },1000);
-            setTimeout(function(){ document.getElementById("dest").innerHTML = data[0].dest; },1500); 
-            setTimeout(function(){ document.getElementById("coach-num").innerHTML = data[0].Coach_num; },1500); 
-            setTimeout(function(){ document.getElementById("seat-num").innerHTML = data[0].Seat_num; },1500); 
-            setTimeout(function(){ document.getElementById("seat-type").innerHTML = data[0].Seat_type; },1500); 
+            setTimeout(function(){ document.getElementById("src").innerHTML = 'Source Station : ' + data[0].src; },1000);
+            setTimeout(function(){ document.getElementById("dest").innerHTML = 'Destination Station : ' + data[0].dest; },1500); 
+            setTimeout(function(){ document.getElementById("coach-num").innerHTML = 'Coach Number : ' + data[0].Coach_num; },1500); 
+            setTimeout(function(){ document.getElementById("seat-num").innerHTML = 'Seat Number : ' +  data[0].Seat_num; },1500); 
+            setTimeout(function(){ document.getElementById("seat-type").innerHTML = 'Seat Type : ' +  data[0].Seat_type; },1500); 
             var url1 = '/gettrainname/'+data[0].train_num;
             var url2 = '/gettraindetail/'+data[0].train_num;
             fetch(url1)
@@ -39,16 +39,44 @@ function fillTicketDetail() {
                         const arr = result[i].arr;
                         const dep = result[i].dep;
                         src_dist = result[i].dist;
-                        setTimeout(function(){ document.getElementById("src-arr").innerHTML = arr; },500); 
-                        setTimeout(function(){ document.getElementById("src-dep").innerHTML = dep; },1000); 
+                        let arr_time = String(Math.ceil(parseInt(arr)/60));
+                        let arr_time_min = parseInt(arr)%60;
+                        if(arr_time_min<10)
+                            arr_time += ':0';
+                        else
+                            arr_time += ':';                       
+                        arr_time+=arr_time_min;
+                        let dep_time = String(Math.ceil(parseInt(dep)/60));
+                        let dep_time_min = parseInt(dep)%60;
+                        if(dep_time_min<10)
+                            dep_time += ':0';
+                        else
+                            dep_time += ':';                       
+                        dep_time+=dep_time_min;
+                        setTimeout(function(){ document.getElementById("src-arr").innerHTML = 'Arrival : ' + arr_time; },500); 
+                        setTimeout(function(){ document.getElementById("src-dep").innerHTML = 'Departure : ' + dep_time; },1000); 
                     }
                     if(result[i].src_station.toLowerCase()===dest.toLowerCase())
                     {
                         const arr = result[i].arr;
                         const dep = result[i].dep;
                         dest_dist = result[i].dist;
-                        setTimeout(function(){ document.getElementById("dest-arr").innerHTML = arr; },500); 
-                        setTimeout(function(){ document.getElementById("dest-dep").innerHTML = dep; },1000);
+                        let arr_time = String(Math.ceil(parseInt(arr)/60));
+                        let arr_time_min = parseInt(arr)%60;
+                        if(arr_time_min<10)
+                            arr_time += ':0';
+                        else
+                            arr_time += ':';                       
+                        arr_time+=arr_time_min;
+                        let dep_time = String(Math.ceil(parseInt(dep)/60));
+                        let dep_time_min = parseInt(dep)%60;
+                        if(dep_time_min<10)
+                            dep_time += ':0';
+                        else
+                            dep_time += ':';                       
+                        dep_time+=dep_time_min;
+                        setTimeout(function(){ document.getElementById("dest-arr").innerHTML = arr_time; },500); 
+                        setTimeout(function(){ document.getElementById("dest-dep").innerHTML = dep_time; },1000);
                     }
                 }
                 setTimeout(function(){ document.getElementById("distance").innerHTML = dest_dist-src_dist; },1000);
@@ -62,6 +90,9 @@ function fillTicketDetail() {
             document.getElementById("src").innerHTML = 'Not Found';
             document.getElementById("dest").innerHTML = 'Not Found';
             document.getElementById("train-name").innerHTML = 'Not Found';
+            document.getElementById("fare").innerHTML = 'Not Found';
+            document.getElementById("distance").innerHTML = 'Not Found';
+            document.getElementById("coach-num").innerHTML = 'Not Found';
         }   
     });
 }
