cb4a4f756fb068d9261a6bc8f133ca175a174d80
varun <varunnssaini@gmail.com>
2021-04-08 21:12:08 +0530

    Changed some CSS

--- views/scripts/template.js
+++ views/scripts/template.js
@@ -1,5 +1,5 @@
-var header = `<header class="w3-container w3-theme w3-padding" id="myHeader">
-<i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button w3-theme"></i>
+var header = `<header class="w3-container w3-teal w3-padding" id="myHeader">
+<i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button w3-teal"></i>
 <div class="w3-center">
   <h4>PROJECT BY VARUN SAINI</h4>
   <h1 class="w3-xxxlarge w3-animate-bottom">RAILWAY MANAGEMENT SYSTEM</h1>
